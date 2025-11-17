e55d8cd688cb91ddc4cf816bbf22f877a1006dd2
Omar Alfarouk <141720964+OmarAlfar0uk@users.noreply.github.com>
2025-11-17 11:13:54 +0200

    Refactor BaseEntity and add MealPlans model
    Changed the `Id` property in `BaseEntity` from `Guid` to `int`
    to standardize the identifier type. Added a new `MealPlans`
    class inheriting from `BaseEntity`, with properties for
    `Name`, `Description`, and `CalorieTarget`. Updated the
    project file to include the `wwwroot` folder in the structure.

--- NutritionService/Domain/Models/BaseEntity.cs
+++ NutritionService/Domain/Models/BaseEntity.cs
@@ -2,7 +2,7 @@
 {
     public class BaseEntity
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public DateTime CreatedAt { set; get; } = DateTime.Now;
         public DateTime? UpdatedAt { set; get; } = DateTime.Now;
         public bool IsDeleted { set; get; } = false;
---
+++ NutritionService/Domain/Models/MealPlans.cs
@@ -0,0 +1,10 @@
+﻿namespace NutritionService.Domain.Models
+{
+    public class MealPlans : BaseEntity 
+    {
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int CalorieTarget { get; set; }
+
+    }
+}
