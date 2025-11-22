8b22d2ac873c582763a2c36b92cdf01bc21cb7fe
Ramez Tawfik <rameztawfik77@gmail.com>
2025-11-22 19:48:05 +0200

    Update Program.cs in FitnessCalculationService

--- FitnessCalculationService/Program.cs
+++ FitnessCalculationService/Program.cs
@@ -22,7 +22,7 @@ namespace FitnessCalculationService
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-
+            //vvv
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
