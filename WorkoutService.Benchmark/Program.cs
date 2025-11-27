4fb81e699768fbd91dce30e75f6a364d30ecda71
Amr Mohamed Shawky <amrshawky936@gmail.com>
2025-11-27 14:25:40 +0200

    tst

---
+++ WorkoutService.Benchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkoutService.Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
